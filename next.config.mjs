87d86edf85765e53ba365f2c2d090e1f465a5c31
Dazz <2798605781@qq.com>
2025-06-10 02:40:21 +0800

    update latest1

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        source: "/api/(.*)", // 覆盖所有API路由
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://learn-next-portfoflo.vercel.app",
+          },
+          { key: "Access-Control-Allow-Methods", value: "POST" },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
